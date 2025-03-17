0f355baa9aab9b915e2cf0087612d450d57139fb
Felix Kube <F.Kube@students.hertie-school.org>
2025-03-17 13:14:48 +0100

    lab 5 afterwork

--- app/flaskapp.py
+++ app/flaskapp.py
@@ -1,37 +1,36 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route("/hello/<city>")
-def hello(city):
-    return f"<h1>Hello, {city}!</h1>"
-
-@app.route('/')
+# this part is new:
+posts = [
+    {
+        'id': 1,
+        'author': 'Your name',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': '4 March 2024'
+    },
+    {
+        'id': 2,
+        'author': 'Your name',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': '5 March 2024'
+    }
+]
+# route for the home page, which is where the blog posts will be shown, so we need to add 'posts=posts
+@app.route("/")
+@app.route("/home")
 def home():
-    return render_template('calc.html')
+    return render_template('home.html', posts=posts)
 
-@app.route('/calculate', methods=['POST'])  # associating the POST method with this route
-def calculate():
 
-    # using the request method from flask to request the values that were sent to the server through the POST method
-    value1 = request.form['value1']
-    value2 = request.form['value2']
-    operation = str(request.form['operation'])
-
-    # convert the input to floating points
-    value1 = float(value1)
-    value2 = float(value2)
-
-    if operation == 'addition':
-        return render_template('calc.html', printed_result=str(value1 + value2))
-    if operation == "substraction":
-        return render_template('calc.html', printed_result=str(value1 - value2))
-    if operation == "multiplication":
-        return render_template('calc.html', printed_result=str(value1 * value2))
-    if operation == "division":
-        return render_template('calc.html', printed_result=str(value1 / value2))
-    else:
-        return render_template('calc.html', printed_result='Operation must be "addition"')
+# this is same as before:
+# route for the about page
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About page')
 
 if __name__ == '__main__':
     app.run(debug=True)
